9358c0dbeb3d590fb76212f00f8c20ef3eed726a
shreyasrama <shreyas.rama1@gmail.com>
2021-04-18 22:00:20 +1200

    Add int parameter to anonymous function

--- 3-golang/main.go
+++ 3-golang/main.go
@@ -9,10 +9,10 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(5)
 	for i := 0; i < 5; i++ {
-		go func() {
-			fmt.Println(i)
+		go func(a int) {
+			fmt.Println(a)
 			wg.Done()
-		}()
+		}(i)
 	}
 	wg.Wait()
 }
